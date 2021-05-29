cef0407b105fef279c4a115bcd0050bc8d5de983
gatupovmikhail <Porog.97@mail.ru>
2021-05-29 20:49:53 +0300

    Окончательный вариант файлов загрузки

--- downloading.sh
+++ downloading.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+echo 'Downloading of .json for example datastet' && \
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip && \
 unzip annotations_trainval2014.zip -d annotations_trainval2014 && \
 rm annotations_trainval2014.zip && \
+echo 'Done' && \
 python3 download_data.py && \
 echo 'Downloading of example dataset' && \
 wget https://data.cyverse.org/dav-anon/iplant/home/mikhailgatupov/example_dataset.zip && \
 unzip example_dataset.zip -d example_dataset && \
 rm example_dataset.zip && \
-echo ' Done' && \ 
+echo ' Done' && \
+echo 'Downloading of model' && \
+wget https://data.cyverse.org/dav-anon/iplant/home/mikhailgatupov/CatPytorchModel.zip  && \
+unzip CatPytorchModel.zip CatPytorchModel && \
+rm CatPytorchModel.zip && \
+echo 'Done' && \
 exit 0
--- downloading_test.sh
+++ downloading_test.sh
@@ -5,6 +5,9 @@ unzip example_dataset.zip -d example_dataset && \
 rm example_dataset.zip && \
 echo ' Done' && \
 echo 'Downloading of model' && \
+wget https://data.cyverse.org/dav-anon/iplant/home/mikhailgatupov/CatPytorchModel.zip  && \
+unzip CatPytorchModel.zip CatPytorchModel && \
+rm CatPytorchModel.zip && \
 echo 'Done' && \
 exit 0
 
