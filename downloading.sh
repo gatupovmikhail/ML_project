be2a20fe44d01b5e4f459c7e6cc99a37e30bf334
gatupovmikhail <Porog.97@mail.ru>
2021-05-29 15:33:43 +0300

    Добавлена загрузка датасета-примера

--- downloading.sh
+++ downloading.sh
@@ -2,4 +2,10 @@
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip && \
 unzip annotations_trainval2014.zip -d annotations_trainval2014 && \
 rm annotations_trainval2014.zip && \
+python3 download_data.py && \
+echo 'Downloading of example dataset' && \
+wget https://data.cyverse.org/dav-anon/iplant/home/mikhailgatupov/example_dataset.zip && \
+unzip example_dataset.zip -d example_dataset && \
+rm example_dataset.zip && \
+echo ' Done' && \ 
 exit 0
