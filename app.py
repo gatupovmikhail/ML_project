91062d77c4018500d51f72459581bc5a9782fce0
gatupovmikhail <Porog.97@mail.ru>
2021-05-29 21:45:27 +0300

    Финальные версии приложения

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ MIN_OVERLAP=16
 NEW_SIZE=128
 BATCH_SIZE = 5
 DIR_OF_MODEL = 'CatPytorchModel'
-name_model = 'base_model'
+name_model = 'model6'
 
 trfm = A.Compose([
     A.Resize(NEW_SIZE,NEW_SIZE),
@@ -93,8 +93,7 @@ def get_model(dir_of_model, name_model):
 
 # Загрузка модели и отправка на GPU
 model = get_model(DIR_OF_MODEL,name_model)
-model.to(DEVICE);
-print('Model was successfully sent to GPU')
+model.to(DEVICE)
 model.eval()
 
 
@@ -131,8 +130,9 @@ def finding_of_cat(name_img, data_dir, window=WINDOW, min_overlap=MIN_OVERLAP,
     return cat_pixels_persentage
 
 
-# Выбор дирректории через окно filebrowser
-foto_dir = './'  # здесь будет хранится выбранная дирректория
+## Выбор дирректории через окно filebrowser
+
+foto_dir = './' # здесь будет хранится выбранная дирректория
 def c_open_dir_old():
     '''Функция для выбора дирректории через filebrowser'''
     dirr = os.getcwd()
@@ -145,7 +145,13 @@ root = tk.Tk()
 style = ttk.Style(root)
 style.theme_use("clam")
 root.configure(bg=style.lookup('TFrame', 'background'))
-ttk.Button(root, text="Open folder", command=c_open_dir_old).grid(row=2, column=0,                                                                   padx=4, pady=4, sticky='ew')
+text_label = 'You should choose directory wih your photos.' + \
+'It"s normal, that you can see only directories (without files) in your directory.'+ \
+'You should go INSIDE your directory (see the entry "Selection")'
+text_window = ttk.Label(root, text=text_label)
+ttk.Button(root, text="Open folder", command=c_open_dir_old).grid(row=2, column=0, \
+                                                                  padx=4, pady=4, sticky='ew')
+text_window.grid(row=1, column=0, padx=4, pady=4)
 # Запуск окна
 root.mainloop()
 
@@ -165,31 +171,45 @@ for f in fotos:
             fotos_filtered.append(f)
     file_counter += 1
 print('Done')
-
 # Переход в указанную дирректорию и создание папки с output:
 old_directory = os.getcwd()
 os.chdir(foto_dir)
 dir_of_cats_foto = 'photos_with_cats'
+dir_without_cats_foto = 'photos_without_cats'
 if not os.path.exists(dir_of_cats_foto):
     os.mkdir(dir_of_cats_foto)
+if not os.path.exists(dir_without_cats_foto):
+    os.mkdir(dir_without_cats_foto)
 
 #  Подготовка принтов для отслеживания процесса  
 number_fotos = len(fotos_filtered)
 foto_counter = 0
-t_work = number_fotos*2
+t_work = number_fotos*5
 if t_work/60 < 1:
     print(f'Оценочное время работы программы: {t_work} с')
+    print('(Может сильно варьироваться в зависимости от размеров фото)')
 else:
     print(f'Оценочное время работы программы: {t_work} c ({int(t_work/60)} min)')
+    print('(Может сильно варьироваться в зависимости от размеров фото)')
 start_time = time.time()
 
 # Проход каждого фото через сеть
 theshold_percentage = 0.20  # какую часть фото должна занимать кошка
 for f in fotos_filtered:
     percent = round(foto_counter/number_fotos,2)
-    print('Обработано фото: {}/{}, {} %'.format(foto_counter, number_fotos,                                            int(percent*100)), end='\r')
-    if finding_of_cat(f, data_dir=foto_dir) >= theshold_percentage:
+    print('Обработано фото: {}/{}, {} %'.format(foto_counter, number_fotos, \
+                                           int(percent*100)), end='\r')
+    try:
+        pers_cat = finding_of_cat(f, data_dir=foto_dir)
+    except (ValueError, RuntimeError):
+        foto_counter += 1
+        print('Что-то не так с фото ', end='')
+        print(f + ' Не могу обработать его.')
+        continue
+    if  pers_cat >= theshold_percentage:
         shutil.copy(f, dir_of_cats_foto)
+    else:
+        shutil.copy(f, dir_without_cats_foto)
     foto_counter += 1
 
 # Информация о прошедшем процессе
@@ -198,4 +218,3 @@ print('Done!')
 print(f'Реальное время работы: {round(time.time() - start_time,2)} c')
 print(f'Фото кошек сохранены в папке {foto_dir + "/" + dir_of_cats_foto}')
 os.chdir(old_directory)
-
