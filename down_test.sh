737821b4e264c28701533c7c205e1e75eefc8acb
gatupovmikhail <Porog.97@mail.ru>
2021-05-29 21:53:15 +0300

    переименованы файлы для загрузки

---
+++ down_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo 'Downloading of example dataset' && \
+wget https://data.cyverse.org/dav-anon/iplant/home/mikhailgatupov/example_dataset.zip && \
+unzip example_dataset.zip -d example_dataset && \
+rm example_dataset.zip && \
+echo ' Done' && \
+echo 'Downloading of model' && \
+wget https://data.cyverse.org/dav-anon/iplant/home/mikhailgatupov/CatPytorchModel.zip  && \
+unzip CatPytorchModel.zip CatPytorchModel && \
+rm CatPytorchModel.zip && \
+echo 'Done' && \
+exit 0
+
